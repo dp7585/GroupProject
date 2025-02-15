cc66ee4a7c26bacf2dbbb967a37281ba06c88413
Your Name <you@example.com>
2025-02-15 13:43:29 +0100

    added new IBarometer.java for question1

---
+++ src/IBarometer.java
@@ -0,0 +1,10 @@
+/*
+ * Esmari louw
+ * An interface for Ibarometer to be able to get used in the barometer class
+ */
+
+public interface IBarometer {
+
+    double pressure(); //any barometer must provide this method
+    
+}
