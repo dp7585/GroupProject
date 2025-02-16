7c1a3defde8c2869d100ef67714fca8d8de1cbab
dp7585 <dp7585@rit.edu>
2025-02-16 05:54:54 +0100

    added the new interface for temperature

---
+++ src/ITempSensor.java
@@ -0,0 +1,8 @@
+/**
+ * Dora Pehar-Ljoljic
+ * Public interface ITempSensor used in KelvinTempSensorAdapter class. This interface is for all temperature sensors
+ */
+
+public interface ITempSensor {
+    double getTemperature(); // Returns temperature for all temperature sensors
+}
