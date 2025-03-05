9f7a6fe31e25f600ffa426024fb46011d1f7fa1a
nataliefraank <nataliefraank@gmail.com>
2025-03-05 12:44:36 +0100

    Added functionality to Food

--- src/Food.java
+++ src/Food.java
@@ -2,6 +2,12 @@ public interface Food {
     // Component
     
     String getFood();
-    void addFood(String food);
+    void addFood(String food, float calories, float fat, float carbs, float protein);
+    void setName(String name);
+    void setCalories(float calories);
+    void setFat(float fat);
+    void setCarbs(float carbs);
+    void setProtein(float protein);
     void display();
+
 }
