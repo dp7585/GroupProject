ec27368a3a50fb5657441d81294331db41e3c913
Your Name <el5333@g.rit.edu>
2025-03-27 10:53:49 +0100

    new dailyLogFodd

---
+++ src/DailyLogFood.java
@@ -0,0 +1,12 @@
+/*
+ * Creating an interface that ensures both FoodItem and Recipe can be used in the same way.
+ * This will make sure that DailyLog can treat them both
+ */
+
+public interface DailyLogFood {
+    String getName();
+    double getCalories();
+    double getProtein();
+    double getCarbs();
+    double getFat();
+}
